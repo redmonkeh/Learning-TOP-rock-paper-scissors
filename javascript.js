074953cb20cab4db27880e845350553437e598d5
redMonkeh <annasmcclellan@gmail.com>
2024-09-30 20:50:27 -0700

    Create getComputerChoice function to randomly generate computer move.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*3);
+    let choiceName;
+    if (choice === 0){
+        choiceName = "Rock";
+        } else if (choice === 1){
+        choiceName = "Paper";
+        }
+        else if (choice === 2){
+        choiceName = "Scissors";
+        }
+        else{
+        choiceName = "Something went wrong...";
+        }
+
+    return choiceName;
+}
+
+console.log(getComputerChoice());
