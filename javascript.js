3b15c0bd9eca11e143b04cd461cb9506324c4836
redMonkeh <annasmcclellan@gmail.com>
2024-10-01 10:34:40 -0700

    Create getHumanChoice to prompt user to enter choice, if invalid prompt again.

--- javascript.js
+++ javascript.js
@@ -19,3 +19,28 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice());
+let promptMessage = "Rock, Paper, or Scissors?";
+
+
+
+function getHumanChoice(){
+    let choiceValidCheck = 0; 
+    let choice = prompt("Rock, Paper, or Scissors?");
+   
+    if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissor" || choice.toLowerCase() === "scissors"){
+            choiceValidCheck = 1;
+    } else {
+        do {
+            choice = prompt("That's not a valid choice, try again. Rock, Paper, or Scissors?");
+            if (choice.toLowerCase() === "rock" || choice.toLowerCase() === "paper" || choice.toLowerCase() === "scissor" || choice.toLowerCase() === "scissors"){
+                choiceValidCheck = 1;
+            } 
+        } while (choiceValidCheck !== 1);
+    }   
+
+    return choice;
+        
+}
+
+console.log(getHumanChoice());
+
